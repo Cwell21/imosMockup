4fef72ff655cad7d11e7429f08b9bacd8cd21127
Cwell21 <cwe11@yahoo.com>
2018-12-15 10:47:56 -0700

    progress commit on slideshow. Created image array

---
+++ js/data.js
@@ -0,0 +1,9 @@
+// DATA FILE FOR IMOS REMAKE
+
+const slideImages = [
+  "images/slides/slide0.jpg",
+  "images/slides/slide1.jpg",
+  "images/slides/slide2.jpg",
+]
+
+export { slideImages }
--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+// JAVASCRIPT FILE FOR IMOS REMAKE
 
+// DATA FOR IMOS REMAKE
+
+const slideImages = [
+  "url('./images/slides/slide0.jpg')",
+  "url('./images/slides/slide1.jpg')",
+]
+
+const el = document.getElementById("slide");
+
+//INITIALIZE SLIDESHOW WITH IIFE 
+(function() {
+  el.style.backgroundImage = slideImages[0];
+}());
+
+function changeSlide() {
+  
+  
+}
+
+setInterval(() => {
+  
+},5000)
