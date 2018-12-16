0e0c08ef003ddfc2485f25a3ed4f2eb13d28edc6
Cwell21 <cwe11@yahoo.com>
2018-12-16 10:14:36 -0700

    slide show showing images.  Work to do on slide markers

--- js/script.js
+++ js/script.js
@@ -5,20 +5,55 @@
 const slideImages = [
   "url('./images/slides/slide0.jpg')",
   "url('./images/slides/slide1.jpg')",
-]
+];
 
+// **********************************************************
+// ************** SLIDE SHOW CODE ***************************
+// **********************************************************
+
+var slideIndex = 0;
+//CREATE CONSTANT FOR SLIDE DIV
 const el = document.getElementById("slide");
+const sqrEl = document.getElementById("square-container");
 
 //INITIALIZE SLIDESHOW WITH IIFE 
 (function() {
+  for (let i=0; i<slideImages.length; i++){
+    let sqr = document.createElement("div");
+    sqr.setAttribute("class", "slide-marker");
+    sqrEl.appendChild(sqr);
+  }
   el.style.backgroundImage = slideImages[0];
+  document.getElementsByClassName("slide-marker")[0].setAttribute("id","marked");
 }());
 
-function changeSlide() {
+//HANDLE SLIDES
+
+function handleSlides() {
+  console.log("handleSlides Called....")
   
+  let slideMarker = document.getElementsByClassName("slide-marker");
   
+
+  function changeSlides() {
+   
+    if (slideIndex < slideImages.length && slideIndex >= 0) {
+      console.log("ChangeSlide called if....")
+      el.style.backgroundImage = slideImages[slideIndex];
+      slideMarker[slideIndex].setAttribute("id", "marked");
+      slideIndex += 1;
+    }else {
+      console.log("ChangeSlide called else....")
+      el.style.backgroundImage = slideImages[0];
+      slideIndex = 0;
+    }
+  }
+  changeSlides();
 }
 
-setInterval(() => {
-  
-},5000)
+//RUN SLIDE SHOW
+setInterval(handleSlides,5000);
+
+// **********************************************************
+// ************** END SLIDE SHOW CODE ***********************
+// **********************************************************
